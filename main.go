1ddc5d9bb9d2c67b493d5aa924ae952a70d53015
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-05-29 07:42:28 +0545

    Get todo by id

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -22,9 +23,41 @@ func getTodos(context *gin.Context) {
 	context.IndentedJSON(http.StatusOK, todos)
 }
 
+func addTodo(context *gin.Context) {
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		return
+	}
+	todos = append(todos, newTodo)
+	fmt.Println("New todo added: ", newTodo)
+	fmt.Println("All todos: ", todos)
+	context.IndentedJSON(http.StatusCreated, todos)
+
+}
+func getTodoById(id string) (*todo, error) {
+	for _, t := range todos {
+		if t.ID == id {
+			return &t, nil
+		}
+	}
+	return nil, fmt.Errorf("todo with ID %s not found", id)
+}
+
+func getTodo(context *gin.Context) {
+	id := context.Param("id")
+	t, err := getTodoById(id)
+	fmt.Println("Todo: ", t)
+	if err != nil {
+		context.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+	context.IndentedJSON(http.StatusOK, t)
+}
 func main() {
 	router := gin.Default()
 	router.GET("/todos", getTodos)
+	router.POST("/todos", addTodo)
+	router.GET("/todos/:id", getTodo)
 	router.Run("localhost:8080")
 
 }
